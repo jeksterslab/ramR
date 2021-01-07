9cc60cb5255ee8560dd560cbb1c5088ef6989103
jeksterslab <social.jeksterslab@gmail.com>
2021-01-07 10:13:04 +0800

    Added package documentation.

---
+++ R/ram.R
@@ -0,0 +1,14 @@
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title ram: Reticular Action Model (RAM) Notation
+#'
+#' @description A collection of functions I use in studying
+#'   the Reticular Action Model (RAM) Notation.
+#'
+#' @section ram functions:
+#'   - [mutheta()] - used to derive the model-implied mean vector
+#'   - [Sigmatheta()] - used to derive the model0implied variance-covariance
+#'
+#' @docType package
+#' @name ram
+NULL
