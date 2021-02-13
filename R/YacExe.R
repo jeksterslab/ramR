b89679ead9af038975055861c9a3c54938829e1f
jeksterslab <social.jeksterslab@gmail.com>
2021-02-14 00:06:18 +0800

    Added YacExe.R.

---
+++ R/YacExe.R
@@ -0,0 +1,73 @@
+#' Execute Yacas Expression using Ryacas
+#'
+#' @references
+#' [YACAS - Yet Another Computer Algebra System](http://www.yacas.org/)
+#'
+#' [Ryacas](http://r-cas.github.io/ryacas/)
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @family Yacas functions
+#' @keywords yac
+#'
+#' @param expr Character string. Yacas expresion.
+#' @param str Logical.
+#' If `TRUE`, returns symbolic result as string.
+#' If `FALSE`, returns symbolic result as an `R` expression.
+#' @param ysym Logical.
+#' If `TRUE`, returns symbolic result as `yac_symbol` if `str = TRUE`.
+#' If `FALSE`, returns symbolic result as string if `str = TRUE`.
+#' @param simplify Logical. Simplify symbolic results.
+#' @param tex Logical. Return results as latex if `str = TRUE`.
+#'
+#' @examples
+#' A <- Ryacas::ysym(
+#'   matrix(c("a", "c", "b", "d"), ncol = 2)
+#' )
+#' expr <- paste0("Determinant(", A, ")")
+#' YacExe(expr)
+#' YacExe(expr, str = TRUE, ysym = FALSE)
+#' YacExe(expr, str = TRUE, tex = TRUE)
+#' YacExe(expr, str = FALSE)
+#'
+#' a <- 1
+#' b <- 2
+#' c <- 3
+#' d <- 4
+#' eval(YacExe(expr, str = FALSE))
+#' @export
+YacExe <- function(expr,
+                   str = TRUE,
+                   ysym = TRUE,
+                   simplify = FALSE,
+                   tex = FALSE) {
+  if (simplify) {
+    expr <- paste0(
+      "Simplify(", expr, ")"
+    )
+  }
+  if (str) {
+    out <- Ryacas::yac_str(expr)
+    if (tex) {
+      return(
+        Ryacas::tex(
+          Ryacas::ysym(out)
+        )
+      )
+    } else {
+      if (ysym) {
+        return(
+          Ryacas::ysym(out)
+        )
+      } else {
+        return(
+          out
+        )
+      }
+    }
+  } else {
+    return(
+      Ryacas::yac_expr(expr)
+    )
+  }
+}
