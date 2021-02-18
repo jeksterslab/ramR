8616fe382ac54020085f5751a43b98c7ad5f0193
jeksterslab <social.jeksterslab@gmail.com>
2021-02-18 16:54:24 +0800

    Automated update.

--- tests/testthat/test_two-variable-regression-Eq2Expectations.R
+++ tests/testthat/test_two-variable-regression-Eq2Expectations.R
@@ -60,34 +60,6 @@ SymbolicParsTex <- ramR::Eq2Expectations(
 #+ SymbolicLabelsTex, results = "asis"
 cat(
   "\\begin{align*}",
-  "\\mathbf{A} = ",
-  SymbolicLabelsTex$A,
-  "\\end{align*}",
-  sep = ""
-)
-cat(
-  "\\begin{align*}",
-  "\\mathbf{S} = ",
-  SymbolicLabelsTex$S,
-  "\\end{align*}",
-  sep = ""
-)
-cat(
-  "\\begin{align*}",
-  "\\mathbf{u} = ",
-  SymbolicLabelsTex$u,
-  "\\end{align*}",
-  sep = ""
-)
-cat(
-  "\\begin{align*}",
-  "\\mathbf{F} = ",
-  SymbolicLabelsTex$Filter,
-  "\\end{align*}",
-  sep = ""
-)
-cat(
-  "\\begin{align*}",
   "\\mathbf{v} = ",
   SymbolicLabelsTex$v,
   "\\end{align*}",
@@ -116,34 +88,6 @@ cat(
 )
 #'
 #+ SymbolicParsTex, results = "asis"
-cat(
-  "\\begin{align*}",
-  "\\mathbf{A} = ",
-  SymbolicParsTex$A,
-  "\\end{align*}",
-  sep = ""
-)
-cat(
-  "\\begin{align*}",
-  "\\mathbf{S} = ",
-  SymbolicParsTex$S,
-  "\\end{align*}",
-  sep = ""
-)
-cat(
-  "\\begin{align*}",
-  "\\mathbf{u} = ",
-  SymbolicParsTex$u,
-  "\\end{align*}",
-  sep = ""
-)
-cat(
-  "\\begin{align*}",
-  "\\mathbf{F} = ",
-  SymbolicParsTex$Filter,
-  "\\end{align*}",
-  sep = ""
-)
 cat(
   "\\begin{align*}",
   "\\mathbf{v} = ",
@@ -224,10 +168,6 @@ g <- as.matrix(
 #' \end{equation}
 #'
 #+ SymbolicLabels
-SymbolicLabelsA <- round(eval(SymbolicLabels$A), digits = 4)
-SymbolicLabelsS <- round(eval(SymbolicLabels$S), digits = 4)
-SymbolicLabelsu <- round(eval(SymbolicLabels$u), digits = 4)
-SymbolicLabelsFilter <- round(eval(SymbolicLabels$Filter), digits = 4)
 SymbolicLabelsv <- round(eval(SymbolicLabels$v), digits = 4)
 SymbolicLabelsg <- round(eval(SymbolicLabels$g), digits = 4)
 SymbolicLabelsC <- round(eval(SymbolicLabels$C), digits = 4)
@@ -240,10 +180,6 @@ p2 <- p[2]
 p3 <- p[3]
 p4 <- p[4]
 p5 <- p[5]
-SymbolicParsA <- round(eval(SymbolicPars$A), digits = 4)
-SymbolicParsS <- round(eval(SymbolicPars$S), digits = 4)
-SymbolicParsu <- round(eval(SymbolicPars$u), digits = 4)
-SymbolicParsFilter <- round(eval(SymbolicPars$Filter), digits = 4)
 SymbolicParsv <- round(eval(SymbolicPars$v), digits = 4)
 SymbolicParsg <- round(eval(SymbolicPars$g), digits = 4)
 SymbolicParsC <- round(eval(SymbolicPars$C), digits = 4)
@@ -262,92 +198,24 @@ NumericLabels <- ramR::Eq2Expectations(eq, par = FALSE)
 NumericPars <- ramR::Eq2Expectations(eq, par = TRUE)
 #'
 #+ NumericLabels
-NumericLabelsA <- round(NumericLabels$A, digits = 4)
-NumericLabelsS <- round(NumericLabels$S, digits = 4)
-NumericLabelsu <- round(NumericLabels$u, digits = 4)
-NumericLabelsFilter <- round(NumericLabels$Filter, digits = 4)
 NumericLabelsv <- round(NumericLabels$v, digits = 4)
 NumericLabelsg <- round(NumericLabels$g, digits = 4)
 NumericLabelsC <- round(NumericLabels$C, digits = 4)
 NumericLabelsM <- round(NumericLabels$M, digits = 4)
 #'
 #+ NumericPars
-NumericParsA <- round(NumericPars$A, digits = 4)
-NumericParsS <- round(NumericPars$S, digits = 4)
-NumericParsu <- round(NumericPars$u, digits = 4)
-NumericParsFilter <- round(NumericPars$Filter, digits = 4)
 NumericParsv <- round(NumericPars$v, digits = 4)
 NumericParsg <- round(NumericPars$g, digits = 4)
 NumericParsC <- round(NumericPars$C, digits = 4)
 NumericParsM <- round(NumericPars$M, digits = 4)
 #'
 #+ round_source
-A <- round(A, digits = 4)
-S <- round(S, digits = 4)
-u <- round(u, digits = 4)
-Filter <- round(Filter, digits = 4)
 C <- round(C, digits = 4)
 M <- round(M, digits = 4)
 v <- round(v, digits = 4)
 g <- round(g, digits = 4)
 #'
 #+ testthat
-testthat::test_that("A.", {
-  for (i in seq_len(nrow(A))) {
-    for (j in seq_len(ncol(A))) {
-      testthat::expect_equal(
-        A[i, j],
-        NumericLabelsA[i, j],
-        NumericParsA[i, j],
-        SymbolicLabelsA[i, j],
-        SymbolicParsA[i, j],
-        check.attributes = FALSE
-      )
-    }
-  }
-})
-testthat::test_that("S.", {
-  for (i in seq_len(nrow(S))) {
-    for (j in seq_len(ncol(S))) {
-      testthat::expect_equal(
-        S[i, j],
-        NumericLabelsS[i, j],
-        NumericParsS[i, j],
-        SymbolicLabelsS[i, j],
-        SymbolicParsS[i, j],
-        check.attributes = FALSE
-      )
-    }
-  }
-})
-testthat::test_that("u.", {
-  for (i in seq_len(nrow(u))) {
-    for (j in seq_len(ncol(u))) {
-      testthat::expect_equal(
-        u[i, j],
-        NumericLabelsu[i, j],
-        NumericParsu[i, j],
-        SymbolicLabelsu[i, j],
-        SymbolicParsu[i, j],
-        check.attributes = FALSE
-      )
-    }
-  }
-})
-testthat::test_that("Filter.", {
-  for (i in seq_len(nrow(Filter))) {
-    for (j in seq_len(ncol(Filter))) {
-      testthat::expect_equal(
-        Filter[i, j],
-        NumericLabelsFilter[i, j],
-        NumericParsFilter[i, j],
-        SymbolicLabelsFilter[i, j],
-        SymbolicParsFilter[i, j],
-        check.attributes = FALSE
-      )
-    }
-  }
-})
 testthat::test_that("C.", {
   for (i in seq_len(nrow(C))) {
     for (j in seq_len(ncol(C))) {
